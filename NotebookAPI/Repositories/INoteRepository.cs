a82b3cfb75ad52ebfda00d733110edbbf4230de2
Alin Banta <banta_alin@yahoo.com>
2018-10-05 11:31:35 +0300

    Added models and implemented note repository

---
+++ NotebookAPI/Repositories/INoteRepository.cs
@@ -0,0 +1,37 @@
+﻿using NotebookAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NotebookAPI.Repositories
+{
+    public interface INoteRepository
+    {
+        Task<IEnumerable<Note>> GetAllNotes();
+
+        Task<Note> GetNote(string id);
+
+        // query after multiple parameters
+        Task<IEnumerable<Note>> GetNote(string bodyText, DateTime updatedFrom, long headerSizeLimit);
+
+        // add new note document
+        Task AddNote(Note item);
+
+        // remove a single document / note
+        Task<bool> RemoveNote(string id);
+
+        Task<bool> RemoveAllNotes();
+
+        // update just a single document
+        Task<bool> UpdateNote(string id, string body);
+
+        // update just a single document
+        Task<bool> UpdateNote(string id, Note item);
+
+        //demo interface - full document update
+        Task<bool> UpdateNoteDocument(string id, string body);
+
+        // creates a sample index
+        Task<string> CreateIndex();
+    }
+}
